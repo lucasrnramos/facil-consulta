eb12e39d564b96fef97baf7de5d1d519b7b6c4a1
lucasrnramos <lucas.rodrigues@unimedmaceio.com.br>
2025-02-03 21:30:09 -0300

    Ajuste do arquivo de rotas

--- apis-facil-consulta/app/Http/Controllers/AuthController.php
+++ apis-facil-consulta/app/Http/Controllers/AuthController.php
@@ -58,7 +58,6 @@ class AuthController extends Controller
     {
         return $this->createNewToken(auth()->refresh());
     }
-
     protected function createNewToken($token): JsonResponse
     {
         return response()->json([
--- apis-facil-consulta/routes/api.php
+++ apis-facil-consulta/routes/api.php
@@ -9,23 +9,29 @@ use App\Http\Controllers\AuthController;
 
 Route::group([
     'middleware' => 'api',
-    'prefix' => 'auth'
-], function ($router) {
-    Route::post('/login', [AuthController::class, 'login']);
-    Route::post('/register', [AuthController::class, 'register']);
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::post('/refresh', [AuthController::class, 'refresh']);
-    Route::get('/user-profile', [AuthController::class, 'userProfile']);
-    Route::post('/verify', [AuthController::class, 'verify']);
+    'prefix'     => 'auth'
+    ], function ($router) {
+        Route::post('/login',       [AuthController::class, 'login']);
+        Route::post('/register',    [AuthController::class, 'register']);
+        Route::post('/logout',      [AuthController::class, 'logout']);
+        Route::post('/refresh',     [AuthController::class, 'refresh']);
+        Route::get('/user-profile', [AuthController::class, 'userProfile']);
+        Route::post('/verify',      [AuthController::class, 'verify']);
 });
 
-//Rota fake para corrigir o problema do redirecionamento quando tentava acessar a api sem enviar o token
+
 Route::get('/login', function () {
-    return response()->json(['status' => 401, 'error' => 'Unauthenticated.'], 401);
+    return response()
+        ->json([
+            'status' => 401,
+            'error'  => 'Não autenticado!'
+        ], 401);
+
 })->name('login');
 
+
 Route::post('usuario/registrar', [AuthController::class, 'register']);
-Route::post('usuario/login', [AuthController::class, 'login']);
+Route::post('usuario/login',     [AuthController::class, 'login']);
 
 Route::group(['middleware' => 'auth:api'], function () {
     Route::get('/cidades', [CidadesController::class, 'retornaCidades']);
