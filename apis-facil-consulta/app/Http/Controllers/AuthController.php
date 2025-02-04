3eac6ddf72095d6af43caa06ad500859d43d9d1e
lucasrnramos <lucas.rodrigues@unimedmaceio.com.br>
2025-02-03 21:00:49 -0300

    Criação de métodos de autenticação

--- apis-facil-consulta/app/Http/Controllers/AuthController.php
+++ apis-facil-consulta/app/Http/Controllers/AuthController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
 
-    public function register(Request $request)
+    public function register(Request $request): JsonResponse
     {
         $user = User::create([
             'name'     => $request->name,
@@ -21,17 +23,81 @@ class AuthController extends Controller
         return response()->json($user, 201);
     }
 
-    public function login(Request $request)
+    public function login(Request $request): JsonResponse
     {
-        $credentials = $request->only('email', 'password');
+        $validador = Validator::make($request->all(), [
+            'email'    => 'required|email',
+            'password' => 'required|string|min:6',
+        ]);
 
-        if (!$token = JWTAuth::attempt($credentials)) {
-            return response()->json(['error' => 'invalid_credentials'], 401);
+        if ($validador->fails()) {
+            return response()->json($validador->errors(), 422);
         }
 
-        return response()->json(['token' => $token]);
+        $credenciais = $request->only('email', 'password');
+
+        if (!$token = JWTAuth::attempt($credenciais)) {
+            return response()->json([
+                'status'  => 401,
+                'success' => false,
+                'msg'     => 'Login e/ou senha inválidos. Acesso não permitido',
+                'data'    => $currentTime,
+            ], 401);
+        }
+
+        return $this->createNewToken($token);
     }
 
+    public function logout(): JsonResponse
+    {
+        auth()->logout();
+        return response()->json(['message' => 'Usuário deslogado com sucesso!']);
+    }
 
+    public function refresh()
+    {
+        return $this->createNewToken(auth()->refresh());
+    }
 
+    protected function createNewToken($token): JsonResponse
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type'   => 'bearer',
+            'expires_in'   => auth('api')->factory()->getTTL() * 60,
+            'user'         => auth('api')->user()
+        ]);
+    }
+
+    public function verify(Request $request): JsonResponse
+    {
+        try {
+            // Verifique se o usuário está autenticado
+            if (!auth()->guard('api')->check()) {
+                return response()->json([
+                    'status'  => 401,
+                    'success' => false,
+                    'msg'     => 'Token inválido. Acesso não permitido.',
+                    'data'    => date('Y-m-d H:i:s')
+                ], 401);
+            }
+
+            // Se o usuário estiver autenticado, retorne um status 200
+            return response()->json([
+                'status'  => 200,
+                'success' => true,
+                'msg'     => 'Token válido.',
+                'data'    => date('Y-m-d H:i:s')
+            ], 200);
+
+        } catch (Exception $e) {
+            // Se houver um erro, retorne um status 500
+            return response()->json([
+                'status'  => 500,
+                'success' => false,
+                'msg'     => 'Erro ao verificar o token.',
+                'data'    => date('Y-m-d H:i:s')
+            ], 500);
+        }
+    }
 }
--- apis-facil-consulta/routes/api.php
+++ apis-facil-consulta/routes/api.php
@@ -6,11 +6,27 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CidadesController;
 use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::get('/user-profile', [AuthController::class, 'userProfile']);
+    Route::post('/verify', [AuthController::class, 'verify']);
+});
+
+//Rota fake para corrigir o problema do redirecionamento quando tentava acessar a api sem enviar o token
+Route::get('/login', function () {
+    return response()->json(['status' => 401, 'error' => 'Unauthenticated.'], 401);
+})->name('login');
 
 Route::post('usuario/registrar', [AuthController::class, 'register']);
 Route::post('usuario/login', [AuthController::class, 'login']);
 
-Route::get('/cidades', [CidadesController::class, 'retornaCidades']);
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::get('/cidades', [CidadesController::class, 'retornaCidades']);
+});
